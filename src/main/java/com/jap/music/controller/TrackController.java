a977ab89a12612fe65ff1f66e12df1ddfc64fcf0
Rishabh Gist <damyrachit@gmail.com>
2022-11-22 19:43:24 +0530

    added TrackService object

--- src/main/java/com/jap/music/controller/TrackController.java
+++ src/main/java/com/jap/music/controller/TrackController.java
@@ -1,10 +1,15 @@
 package com.jap.music.controller;
 
+import com.jap.music.service.TrackService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
 public class TrackController {
+    private TrackService trackService;
 
+    public TrackController(TrackService trackService) {
+        this.trackService = trackService;
+    }
 }
