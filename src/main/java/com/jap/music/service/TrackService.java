63bb7277585e3af38eb0847568561378e036ee65
Rishabh Gist <damyrachit@gmail.com>
2022-11-22 19:13:59 +0530

    added service annotation and TrackRepository object

--- src/main/java/com/jap/music/service/TrackService.java
+++ src/main/java/com/jap/music/service/TrackService.java
@@ -1,4 +1,13 @@
 package com.jap.music.service;
 
+import com.jap.music.repository.TrackRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TrackService {
+    private TrackRepository trackRepository;
+
+    public TrackService(TrackRepository trackRepository) {
+        this.trackRepository = trackRepository;
+    }
 }
