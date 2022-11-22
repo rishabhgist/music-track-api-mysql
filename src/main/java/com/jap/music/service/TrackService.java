a8b73aaf5855ce4be2cf5017dc548f12dab7ad69
Rishabh Gist <damyrachit@gmail.com>
2022-11-22 19:15:52 +0530

    implemented ITrackService in TrackService

--- src/main/java/com/jap/music/service/TrackService.java
+++ src/main/java/com/jap/music/service/TrackService.java
@@ -1,14 +1,47 @@
 package com.jap.music.service;
 
+import com.jap.music.domain.Track;
 import com.jap.music.repository.TrackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class TrackService {
+public class TrackService implements ITrackService {
     private TrackRepository trackRepository;
     @Autowired
     public TrackService(TrackRepository trackRepository) {
         this.trackRepository = trackRepository;
+    }
+
+    @Override
+    public List<Track> getAll() {
+        return null;
+    }
+
+    @Override
+    public Track save(Track track) {
+        return null;
+    }
+
+    @Override
+    public List<Track> getTrackByName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<Track> getTrackByArtist(String artist) {
+        return null;
+    }
+
+    @Override
+    public Track update(int id, Track track) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return false;
     }
 }
