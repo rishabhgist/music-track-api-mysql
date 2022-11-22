83998f60276b7be6d18e99ffcfb984e431d85362
Rishabh Gist <damyrachit@gmail.com>
2022-11-22 17:53:12 +0530

    added no args and parametrized constructor

--- src/main/java/com/jap/music/domain/Track.java
+++ src/main/java/com/jap/music/domain/Track.java
@@ -6,4 +6,15 @@ public class Track {
     private String comments;
     private double rating;
     private String artist;
+
+    public Track() {
+    }
+
+    public Track(int id, String name, String comments, double rating, String artist) {
+        this.id = id;
+        this.name = name;
+        this.comments = comments;
+        this.rating = rating;
+        this.artist = artist;
+    }
 }
