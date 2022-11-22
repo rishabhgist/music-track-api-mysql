8fd7209c08f983c070273e77395c46a58b6b5a94
Rishabh Gist <damyrachit@gmail.com>
2022-11-22 19:14:17 +0530

    linked with autowired

--- src/main/java/com/jap/music/service/TrackService.java
+++ src/main/java/com/jap/music/service/TrackService.java
@@ -1,12 +1,13 @@
 package com.jap.music.service;
 
 import com.jap.music.repository.TrackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TrackService {
     private TrackRepository trackRepository;
-
+    @Autowired
     public TrackService(TrackRepository trackRepository) {
         this.trackRepository = trackRepository;
     }
