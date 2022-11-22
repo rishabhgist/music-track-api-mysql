7090bb976fb25a8af84d820baa33c4aaf1cf8251
Rishabh Gist <damyrachit@gmail.com>
2022-11-22 19:12:15 +0530

    added abstract methods in ITrackService interface

--- src/main/java/com/jap/music/service/ITrackService.java
+++ src/main/java/com/jap/music/service/ITrackService.java
@@ -1,4 +1,14 @@
 package com.jap.music.service;
 
+import com.jap.music.domain.Track;
+
+import java.util.List;
+
 public interface ITrackService {
+    List<Track> getAll();
+    Track save(Track track);
+    List<Track> getTrackByName(String name);
+    List<Track> getTrackByArtist(String artist);
+    Track update(int id, Track track);
+    boolean delete(int id);
 }
