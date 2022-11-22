fff9a16ee1e2d8329d8545106fafbbe28715f557
Rishabh Gist <damyrachit@gmail.com>
2022-11-22 18:05:25 +0530

    added abstract method in TrackRepository class

--- src/main/java/com/jap/music/repository/TrackRepository.java
+++ src/main/java/com/jap/music/repository/TrackRepository.java
@@ -3,5 +3,10 @@ package com.jap.music.repository;
 import com.jap.music.domain.Track;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TrackRepository extends JpaRepository<Track, Integer> {
+    List<Track> findByTrackName(String name);
+    List<Track> findByArtistName(String artist);
+
 }
