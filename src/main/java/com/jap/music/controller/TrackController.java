24d17754382ce1476694a7ef9bdc5157cfeb584e
Rishabh Gist <damyrachit@gmail.com>
2022-11-22 19:43:54 +0530

    added Autowired to link Service and repository

--- src/main/java/com/jap/music/controller/TrackController.java
+++ src/main/java/com/jap/music/controller/TrackController.java
@@ -1,6 +1,7 @@
 package com.jap.music.controller;
 
 import com.jap.music.service.TrackService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class TrackController {
     private TrackService trackService;
-
+    @Autowired
     public TrackController(TrackService trackService) {
         this.trackService = trackService;
     }
