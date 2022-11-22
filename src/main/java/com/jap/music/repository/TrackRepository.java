ffc12b67ea8d34dcab53a71c5dcca738b2db5dcf
Rishabh Gist <damyrachit@gmail.com>
2022-11-22 17:57:37 +0530

    added implemented JpaRepository to TrackRepository

--- src/main/java/com/jap/music/repository/TrackRepository.java
+++ src/main/java/com/jap/music/repository/TrackRepository.java
@@ -1,4 +1,7 @@
 package com.jap.music.repository;
 
-public interface TrackRepository {
+import com.jap.music.domain.Track;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrackRepository extends JpaRepository<Track, Integer> {
 }
