8fec26284e5863ad465f6121769f136db52cac73
Rishabh Gist <damyrachit@gmail.com>
2022-11-22 17:52:23 +0530

    added fields in Track class

--- src/main/java/com/jap/music/domain/Track.java
+++ src/main/java/com/jap/music/domain/Track.java
@@ -1,4 +1,9 @@
 package com.jap.music.domain;
 
 public class Track {
+    private int id;
+    private String name;
+    private String comments;
+    private double rating;
+    private String artist;
 }
