7b2dcd79ed34e07cc32ad121c1f743f2cc8f6b82
Rishabh Gist <damyrachit@gmail.com>
2022-11-22 20:08:22 +0530

    added postMapping

--- src/main/java/com/jap/music/controller/TrackController.java
+++ src/main/java/com/jap/music/controller/TrackController.java
@@ -1,13 +1,11 @@
 package com.jap.music.controller;
 
+import com.jap.music.domain.Track;
 import com.jap.music.service.TrackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1")
@@ -24,5 +22,9 @@ public class TrackController {
     @GetMapping("/track/{id}")
     public ResponseEntity<?> getById(@PathVariable int id){
         return new ResponseEntity<>(trackService.getById(id), HttpStatus.OK);
+    }
+    @PostMapping("/post")
+    public ResponseEntity<?> save(@RequestBody Track track){
+        return new ResponseEntity<>(trackService.save(track), HttpStatus.CREATED);
     }
 }
